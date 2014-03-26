5a8940a9f8aeb945eb365383ff6a65b143181f43
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-03-26 18:00:02 -0300

    remove timout alert

--- js/app/settings.js
+++ js/app/settings.js
@@ -8,30 +8,28 @@ meumobiSettings.constant('TIMEOUT', 10000);
 meumobiSettings.constant('ITEM_PER_PAGE', 20);
 
 meumobiSettings.factory('utils', ['$location', 'API_URL', 'DOMAIN',
-  function($location, API_URL, DOMAIN){
-    var utils = {};
-    utils.getApiUrl = function() {
-      if (typeof DOMAIN != 'undefined') {
-        return API_URL + '/api/' + DOMAIN;
-      }
-      return API_URL + '/api/' + $location.host();
-    };
-    return utils;
-  }]);
+		function($location, API_URL, DOMAIN){
+			var utils = {};
+			utils.getApiUrl = function() {
+				if (typeof DOMAIN != 'undefined') {
+					return API_URL + '/api/' + DOMAIN;
+				}
+				return API_URL + '/api/' + $location.host();
+			};
+			return utils;
+		}]);
 
 meumobiSettings.factory('errorHttpInterceptor', function($q) {
-	  return {
-	   'requestError': function(rejection) {
-	      // do something on error
-	      alert('Sorry an error occured, please try again later');
-	      console.log(rejection);
-	      return $q.reject(rejection);
-	    },
-	   'responseError': function(rejection) {
-	      // do something on error
-		  alert('Sorry an error occured, please try again later');
-		  console.log(rejection);
-		  return $q.reject(rejection);
-	    }
-	  };
-	});
+	return {
+		'requestError': function(rejection) {
+			// do something on error
+			console.log(rejection);
+			return $q.reject(rejection);
+		},
+	'responseError': function(rejection) {
+		// do something on error
+		console.log(rejection);
+		return $q.reject(rejection);
+	}
+	};
+});
