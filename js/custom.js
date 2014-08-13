9a08423c94ec25facd76fc69a213f67abee68e2a
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-08-13 10:14:58 -0300

    update banner

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,10 @@
   });
   if ( !(/(iPad|iPhone|iPod).*OS [6-7].*AppleWebKit.*Mobile.*Safari/.test(navigator.userAgent)) ) {
     console.log('cacete');
-    $.smartbanner();
+    $.smartbanner({
+      daysHidden: 0,
+      daysReminde: 0
+    });
   }
 
 }(window.jQuery);
