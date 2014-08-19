d28d12742f9d9321f8562f43f6c369ae285eacdb
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-08-19 10:14:11 -0300

    update conpress

--- compress.sh
+++ compress.sh
@@ -5,6 +5,8 @@ cat js/lib/angular-file-upload-shim.min.js js/lib/angular/angular.min.js js/lib/
   js/lib/angular-translate-handler-log.min.js	js/lib/angular-slugify.js js/lib/truncate.js \
   js/lib/angulartics.js js/lib/angulartics-ga.js \
 	js/lib/angular-media-player.min.js \
+  # if satander
+  #js/lib/aes.js js/lib/md5.js js/santander_config.js \
 	js/app/helpers.js js/app/services.js js/app/controllers.js js/app/directives.js js/app/settings.js \
 	js/app/bootstrap.js js/bootstrap.js js/parsley/parsley.min.js \
 	js/jquery.blueimp-gallery.min.js js/app.js js/app.plugin.js \
