d74688854e3f2b01c7281dd368cd80c490010310
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2015-02-26 12:14:41 -0300

    show file size

--- js/app/filters.js
+++ js/app/filters.js
@@ -19,3 +19,12 @@ meumobiFilters.filter('mediaClickLabel', function(MEDIAS_ICONS_AND_LABELS) {
     return MEDIAS_ICONS_AND_LABELS[type] ? MEDIAS_ICONS_AND_LABELS[type].label : 'open';
   };
 });
+meumobiFilters.filter('bytesToSize', function() {
+  return function(bytes) {
+    if(bytes == 0) return '0 Byte';
+    var k = 1000;
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+  };
+});
