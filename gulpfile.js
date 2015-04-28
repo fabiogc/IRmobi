d236378ce9ad13aab49f762107a076a747bd8533
Tadeu Valentt <tadeu.valentt@gmail.com>
2015-04-28 13:36:23 -0300

    add gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,168 @@
 var config = {
-  language: 'pt',
-  build: [
-    'js/' 
-  ]
+  dest: 'www',
+  vendor: {
+    js: [
+      './bower_components/jquery/dist/jquery.js',
+      './bower_components/bootstrap/js/bootstrap.js',
+      './src/js/lib/angular-file-upload-shim.min.js',
+      './bower_components/angular/angular.js',
+      './bower_components/angular-route/angular-route.js',
+      './bower_components/angular-touch/angular-touch.js',
+      './bower_components/angular-sanitize/angular-sanitize.js',
+      './bower_components/angular-cookies/angular-cookies.js',
+      './bower_components/angular-resource/angular-resource.js',
+      
+      './bower_components/angulartics/src/angulartics.js',
+      './bower_components/angulartics/src/angulartics-ga.js',
+      './bower_components/angular-translate/angular-translate.js',
+      
+      './src/js/lib/angular-file-upload.min.js',
+      './src/js/lib/angularLocalStorage.js',
+      './src/js/lib/http-with-fallback.js',
+      './src/js/lib/angular-slugify.js',
+      './src/js/lib/angular-calendar.js',
+      './src/js/lib/truncate.js',
+      './src/js/lib/md5.min.js',
+      './src/js/lib/angular-pushwoosh.js',
+      './src/js/lib/angular-adtech.js',
+      './src/js/lib/angular-media-player.min.js'
+    ],
+    css: [
+      './bower_components/bootstrap/dist/css/bootstrap.css',
+      './bower_components/font-awesome/css/font-awesome.css'
+    ],
+    fonts: [
+      './bower_components/font-awesome/fonts/fontawesome-webfont.*'
+    ]
+  }
 };
 
-var gulp           = require('gulp'),
-    request        = require("request"),
-    fs             = require('fs'), 
-    minimist       = require('minimist'),
-    extend         = require('util')._extend,
-    del            = require('del'),
-    zip            = require('gulp-zip'),
-    path           = require('path');
+var gulp = require('gulp'),
+    seq = require('run-sequence'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssmin = require('gulp-cssmin'),
+    order = require('gulp-order'),
+    concat = require('gulp-concat'),
+    ignore = require('gulp-ignore'),
+    rimraf = require('gulp-rimraf'),
+    templateCache = require('gulp-angular-templatecache'),
+    replace = require('gulp-replace'),
+    ngFilesort = require('gulp-angular-filesort'),
+    streamqueue = require('streamqueue'),
+    rename = require('gulp-rename'),
+    path = require('path');
 
-//TODO fix symlink bug
-/*if (fs.existsSync('./config.json')) {
-  var cf = require('./config.json');
-  extend(config, cf);
-};*/
-//get cli parameters
-extend(config, minimist(process.argv.slice(2)));
+/*================================================
+=            Report Errors to Console            =
+================================================*/
+
+gulp.on('err', function(e) {
+  console.log(e.err.stack);
+});
+
+
+/*=========================================
+=            Clean dest folder            =
+=========================================*/
+
+gulp.task('clean', function(cb) {
+  return gulp.src([
+      path.join(config.dest, 'index.html'),
+      path.join(config.dest, 'images'),
+      path.join(config.dest, 'css'),
+      path.join(config.dest, 'js'),
+      path.join(config.dest, 'fonts')
+    ], {
+      read: false
+    })
+    .pipe(rimraf());
+});
+
+
+/*==================================
+=            Copy fonts            =
+==================================*/
+
+gulp.task('fonts', function() {
+  return gulp.src(config.vendor.fonts)
+    .pipe(gulp.dest(path.join(config.dest, 'fonts')));
+});
+
+
+/*=================================================
+=            Copy html files to dest              =
+=================================================*/
 
 gulp.task('html', function() {
-  return request(config.url)
-    .pipe(fs.createWriteStream(config.language + "/main.html"));
+  var inject = [];
+  if (typeof config.weinre === 'object') {
+    inject.push('<script src="http://' + config.weinre.boundHost + ':' + config.weinre.httpPort + '/target/target-script-min.js"></script>');
+  }
+  if (config.cordova) {
+    inject.push('<script src="cordova.js"></script>');
+  }
+  gulp.src(['src/html/**/*.html'])
+    .pipe(replace('<!-- inject:js -->', inject.join('\n    ')))
+    .pipe(gulp.dest(config.dest));
 });
 
-/*gulp.task('compress', function() {
-  console.log(process.cwd());
-  //TODO remove unnecessary files
-  return gulp.src('pt/*', {cwd: process.cwd()})
-        .pipe(zip('pt.zip'));
-});*/
 
-gulp.task('clean_app', function(cb) {
-  del([config.language + '/main.html'], cb)
+/*======================================================================
+=            Compile, minify css                                       =
+======================================================================*/
+
+gulp.task('css', function() {
+    streamqueue({
+        objectMode: true
+      },
+      gulp.src(config.vendor.css),
+      gulp.src('./src/css/**/*.css')
+    )
+    .pipe(concat('app.css'))
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(path.join(config.dest, 'css')));
 });
 
-gulp.task('build', ['clean_app'], function() {
-  gulp.start('html');
+/*====================================================================
+=            Compile and minify js generating source maps            =
+====================================================================*/
+// - Orders ng deps automatically
+// - Precompile templates to ng templateCache
+
+gulp.task('js', function() {
+  streamqueue({
+        objectMode: true
+      },
+      gulp.src(config.vendor.js),
+      gulp.src('./src/js/app/**/*.js').pipe(ngFilesort()),
+      gulp.src('./src/js/theme/**/*.js').pipe(ngFilesort())
+    )
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    //.pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(path.join(config.dest, 'js')));
+});
+
+gulp.task('build', function(done) {
+  var tasks = ['html', 'fonts', 'css', 'js'];
+  seq('clean', tasks, done);
+});
+
+/*====================================
+=            Default Task            =
+====================================*/
+
+gulp.task('default', function(done) {
+  var tasks = [];
+
+  seq('build', done);
 });
--- src/js/santander_config.js
+++
@@ -1,4 +0,0 @@
-config_data.SITEBUILDER = CryptoJS.AES.decrypt('U2FsdGVkX1+NCYaZJylfPETkCIrNg7X6OpzaSTuAFjHO8UvY3FOqZ3bF56ZCjjsF', '9456bbf53af6fdf30a5d625ebf155b4018c8b0aephp').toString(CryptoJS.enc.Utf8);
-if (CryptoJS.MD5(config_data.SITEBUILDER) != '776e69d29f594c3619b67dce220ce48e') {
-	config_data.SITEBUILDER = '';
-}
